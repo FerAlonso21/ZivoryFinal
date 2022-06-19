1983424a813e85cab8137eec5843e1085bf1da7f
Feer-Cano <83847899+Feer-Cano@users.noreply.github.com>
2022-06-18 21:29:40 -0500

    registro
    ñoño

--- src/app/General/Servicios/user.service.ts
+++ src/app/General/Servicios/user.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword,signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor( private auth:Auth) { }
+  constructor( private auth:Auth,private afauth:AngularFireAuth) { }
 
-  registrar({email,password}: any){
-      return createUserWithEmailAndPassword(this.auth,email,password);
+  async registrar(email:string,password:string){
+    try{
+      return await this.afauth.createUserWithEmailAndPassword(email,password);
+    }catch(err){
+      console.log("Error en registro", err);
+      return null;
+    }
   }
 
   login({email,password}:any){
