7b921f900f79171120ffb82f5af68f1180d97e5c
charliram <83847746+charliram@users.noreply.github.com>
2022-06-19 03:53:49 -0500

    v5
    charly v5

--- src/app/Miembros/renta-miembro/renta-miembro.component.ts
+++ src/app/Miembros/renta-miembro/renta-miembro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PrincipalService } from 'src/app/Administrador/Servicios/principal.service';
+import Principal from 'src/app/Interfaces/Principal.interface';
 
 @Component({
   selector: 'app-renta-miembro',
@@ -6,46 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./renta-miembro.component.css']
 })
 export class RentaMiembroComponent implements OnInit {
-
-  array: any[] = [
-  
-      {
-      "nombre":"Mustang",
-      "marca":"Ford",
-      "anio":"2016",
-      "tipo":"Coupe",
-      "color":"Rojo",
-      "url":"https://static.cargurus.com/images/forsale/2021/10/26/04/10/2016_ford_mustang-pic-4656298418775849549-1024x768.jpeg?io=true&width=640&height=480&fit=bounds&format=jpg&auto=webp"
-    },
-    {
-      "nombre":"Corolla",
-      "marca":"Toyota",
-      "anio":"2015",
-      "tipo":"Hatchback",
-      "color":"Blanco",
-      "url":"https://toyotaapizaco.com.mx/panelautos/wp-content/uploads/2019/09/WhatsApp-Image-2019-09-09-at-18.40.32.jpeg"
-    },
-    {
-      "nombre":"Jetta",
-      "marca":"VW",
-      "anio":"2018",
-      "tipo":"Sedan",
-      "color":"Gris",
-      "url":"https://soloautos.li.csnstatic.com/soloautos/cars/private/6gykd7dv39fnmwku5doso70oj.jpg?pxc_method=gravityfill&pxc_bgtype=self&height=725&width=1087"
-    },
-    {
-      "nombre":"Focus",
-      "marca":"Ford",
-      "anio":"2017",
-      "tipo":"Hatchback",
-      "color":"Blanco",
-      "url":"https://toyotaapizaco.com.mx/panelautos/wp-content/uploads/2019/09/WhatsApp-Image-2019-09-09-at-18.40.32.jpeg"
-    },
-  
-  ];
-  constructor() { }
+  principal:Principal[]=[];
+  array:Principal[] = [];
+  constructor(private servicio:PrincipalService) { }
 
   ngOnInit(): void {
+    
+    this.servicio.getAutos().subscribe(principal =>{
+      console.log(principal);
+      this.principal=principal;
+      
+      for(let i of this.principal){
+          
+        if(i.disponibilidad=="true"){
+            this.array.push(i);
+        }
+      }
+    })
     console.log(this.array);
   }
 
